a873892947d7c5b59399d80882a5d47eddbbf9d4
Loai <loai.zaidan@axsos.academy>
2024-12-03 13:15:43 +0200

    new additions

---
+++ Java Assignments/First Java Program/FirstProgram.java
@@ -0,0 +1,7 @@
+public class FirstProgram{
+	public static void main(String[] args){
+		System.out.println("My name is Loai Zaidan");
+		System.out.println("I am 20 years old");
+		System.out.println("My hometown is Ramallah, Palestine");
+	}
+}
