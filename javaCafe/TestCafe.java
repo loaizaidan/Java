134c9a7457fd434664380f41ab3634057b4a7fce
Loai <loai.zaidan@axsos.academy>
2024-12-05 14:52:44 +0200

    "Adding new asgnmnt"

---
+++ javaCafe/TestCafe.java
@@ -0,0 +1,34 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+public class TestCafe {
+    public static void main(String[] args) {
+        
+        CafeUtil myCafe = new CafeUtil();
+        
+        /* ============ App Test Cases ============= */
+    
+        System.out.println("\n----- Streak Goal Test -----");
+        System.out.printf("Purchases needed by week 10: %s \n\n", myCafe.getStreakGoal());
+    
+        System.out.println("----- Order Total Test-----");
+        double[] lineItems = {3.5, 1.5, 4.0, 4.5};
+        System.out.printf("Order total: %s \n\n",myCafe.getOrderTotal(lineItems));
+        
+        System.out.println("----- Display Menu Test-----");
+        
+        ArrayList<String> menu = new ArrayList<String>();
+        menu.add("drip coffee");
+        menu.add("cappuccino");
+        menu.add("latte");
+        menu.add("mocha");
+        myCafe.displayMenu(menu);
+    
+        System.out.println("\n----- Add Customer Test-----");
+        ArrayList<String> customers = new ArrayList<String>();
+        // --- Test 10 times ---
+        for (int i = 0; i < 10; i++) {
+            myCafe.addCustomer(customers);
+            System.out.println("\n");
+        }
+    }
+}
